1d31a6681c78d6e8cbf39482f16900fd27eef62d
Daniel Ale <danielale9291@gmail.com>
2018-11-17 07:37:37 +0100

    refactored config

--- config/keys.js
+++ config/keys.js
@@ -8,23 +8,7 @@ const config = {
 		projectId: process.env.projectId,
 		storageBucket: process.env.storageBucket,
 		messagingSenderId: process.env.messagingSenderId
-
-		// apiKey: "AIzaSyBTgTVUeuz7f6TFj1Hc-BOxTffYf58-EAE",
-		// authDomain: "zip-dance.firebaseapp.com",
-		// databaseURL: "https://zip-dance.firebaseio.com",
-		// projectId: "zip-dance",
-		// storageBucket: "zip-dance.appspot.com",
-		// messagingSenderId: "356081457910"
 	}
 };
 
 module.exports = config;
-
-// var testConfig = {
-// 	apiKey: process.env.apiKey,
-// 	authDomain: process.env.authDomain,
-// 	databaseURL: process.env.databaseURL,
-// 	projectId: process.env.projectId,
-// 	storageBucket: process.env.storageBucket,
-// 	messagingSenderId: process.env.messagingSenderId
-// };
