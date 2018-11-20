77fb79bdec8a33507ab354c01ad3b221bdb18ad4
Daniel Ale <danielale9291@gmail.com>
2018-11-20 08:07:57 +0100

    Deploy API

--- config/keys.js
+++ config/keys.js
@@ -1,13 +1,21 @@
 require("dotenv").config();
 
 const config = {
-	firebaseConfig: {
+	firebaseConfig2: {
 		apiKey: process.env.apiKey,
 		authDomain: process.env.authDomain,
 		databaseURL: process.env.databaseURL,
 		projectId: process.env.projectId,
 		storageBucket: process.env.storageBucket,
 		messagingSenderId: process.env.messagingSenderId
+	},
+	firebaseConfig: {
+		apiKey: "AIzaSyBTgTVUeuz7f6TFj1Hc-BOxTffYf58-EAE",
+		authDomain: "zip-dance.firebaseapp.com",
+		databaseURL: "https://zip-dance.firebaseio.com",
+		projectId: "zip-dance",
+		storageBucket: "zip-dance.appspot.com",
+		messagingSenderId: "356081457910"
 	},
 	cloudinaryConfig: {
 		cloud_name: "dm9ykdajq",
