477c3999b45000a709041e93bcbe6d62f0cc46c9
Daniel Ale <danielale9291@gmail.com>
2018-11-21 07:55:46 +0100

    Image Uploads

--- config/keys.js
+++ config/keys.js
@@ -9,14 +9,6 @@ const config = {
 		storageBucket: process.env.storageBucket,
 		messagingSenderId: process.env.messagingSenderId
 	},
-	firebaseConfig: {
-		apiKey: "AIzaSyBTgTVUeuz7f6TFj1Hc-BOxTffYf58-EAE",
-		authDomain: "zip-dance.firebaseapp.com",
-		databaseURL: "https://zip-dance.firebaseio.com",
-		projectId: "zip-dance",
-		storageBucket: "zip-dance.appspot.com",
-		messagingSenderId: "356081457910"
-	},
 	cloudinaryConfig: {
 		cloud_name: "dm9ykdajq",
 		api_key: "249255527367216",
