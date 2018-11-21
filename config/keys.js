cee3c8afba5c5bcc5bad3a09cd61a340e2c781ce
Daniel Ale <danielale9291@gmail.com>
2018-11-21 07:56:31 +0100

    Uploads

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,14 @@
 require("dotenv").config();
 
 const config = {
+	firebaseConfig: {
+		apiKey: "AIzaSyBTgTVUeuz7f6TFj1Hc-BOxTffYf58-EAE",
+		authDomain: "zip-dance.firebaseapp.com",
+		databaseURL: "https://zip-dance.firebaseio.com",
+		projectId: "zip-dance",
+		storageBucket: "zip-dance.appspot.com",
+		messagingSenderId: "356081457910"
+	},
 	firebaseConfig2: {
 		apiKey: process.env.apiKey,
 		authDomain: process.env.authDomain,
